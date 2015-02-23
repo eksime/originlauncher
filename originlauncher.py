2785057a7577aa834c6bdb3bd20c1c51d87be944
Elliot Sime <e.sime@lancs.ac.uk>
2015-02-23 03:08:08 +0000

    Update build

--- originlauncher.py
+++ originlauncher.py
@@ -1,18 +1,29 @@
 from winreg import *
 from os import getenv
+from subprocess import Popen
+from shlex import split
 from xml.dom import minidom
+import psutil 
+
+from time import sleep
 
 aKey = OpenKey(HKEY_LOCAL_MACHINE, r"SOFTWARE\Wow6432Node\Electronic Arts\EA Core")
-originDir = QueryValueEx(aKey , r"EADM6InstallDir")[0]
+originDir = QueryValueEx(aKey , r"EADM6InstallDir")[0] + "\\"
 CloseKey(aKey)  
    
 xml = minidom.parse(getenv("APPDATA") + r"\origin\local.xml")
 itemlist = xml.getElementsByTagName('Setting')
 
 for s in itemlist :
-    if s.attributes["key"].value == "DownloadInPlaceDir" :
+    if s.attributes["key"].value == "DownloadInPlaceDir":
         originGamesDir = s.attributes["value"].value
         break
 
-print(originDir) 
-print(originGamesDir) 
+proc = Popen([originDir + "Origin.exe", r"origin://launchgame/OFB-EAST:43437"])
+origin = psutil.Process(proc.pid)
+
+while len(origin.get_children()) == 0:
+    sleep(0.1)
+
+origin.get_children()[0].wait()
+origin.kill()
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 import py2exe
+import sys
 
 sys.argv.append('py2exe')
 
 setup(
+
     options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
-    console=['originlauncher.py'],
+    windows=['originlauncher.py'],
     zipfile = None
 )

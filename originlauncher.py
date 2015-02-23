45b896887e2d53c6ae2b9c74a18812d40ac0ec80
Elliot Sime <e.sime@lancs.ac.uk>
2015-02-23 17:33:17 +0000

    Smaller imports, remove XML

--- originlauncher.py
+++ originlauncher.py
@@ -1,18 +1,18 @@
-from winreg import *
+from winreg import OpenKey, CloseKey, QueryValueEx, HKEY_LOCAL_MACHINE
 from os import getenv
 from subprocess import Popen
 from shlex import split
-from xml.dom import minidom
-import psutil 
+#from xml.dom import minidom
+from psutil import Process
 
 from time import sleep
 
-aKey = OpenKey(HKEY_LOCAL_MACHINE, r"SOFTWARE\Wow6432Node\Electronic Arts\EA Core")
-originDir = QueryValueEx(aKey , r"EADM6InstallDir")[0] + "\\"
-CloseKey(aKey)  
+regKey = OpenKey(HKEY_LOCAL_MACHINE, r"SOFTWARE\Wow6432Node\Electronic Arts\EA Core")
+originDir = QueryValueEx(regKey , r"EADM6InstallDir")[0] + "\\"
+CloseKey(regKey)  
    
-xml = minidom.parse(getenv("APPDATA") + r"\origin\local.xml")
-itemlist = xml.getElementsByTagName('Setting')
+#xml = minidom.parse(getenv("APPDATA") + r"\origin\local.xml")
+#itemlist = xml.getElementsByTagName('Setting')
 
 for s in itemlist :
     if s.attributes["key"].value == "DownloadInPlaceDir":
@@ -20,7 +20,7 @@ for s in itemlist :
         break
 
 proc = Popen([originDir + "Origin.exe", r"origin://launchgame/OFB-EAST:43437"])
-origin = psutil.Process(proc.pid)
+origin = Process(proc.pid)
 
 while len(origin.get_children()) == 0:
     sleep(0.1)
